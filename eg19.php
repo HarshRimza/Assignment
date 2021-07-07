ab5bb28c03703ced767814e77939e572a0f7ffb0
Harsh Rimza <60133190+HarshRimza@users.noreply.github.com>
2021-07-07 18:32:21 +0530

    Add files via upload

---
+++ eg19.php
@@ -0,0 +1,36 @@
+<?php
+// Incomplete Code
+
+$connection=new mysqli("localhost:3306","root","harsh","me");
+if($connection->connect_error) die("Connection failed: ".$connection->connect_error);
+
+$statement="select 100*(select count(*) from student where gender='M')/count(*) as male_percent from student";
+$executionTime=microtime(true);
+$result=$connection->query($statement);
+$executionTime=number_format((microtime(true)-$executionTime),10);
+if(mysqli_num_rows($result)>0)
+{
+$rows=mysqli_fetch_all($result);
+$howManyMale=$row['male_percent'];
+echo("$howManyMale\n");
+echo("$executionTime\n");
+}
+else
+{
+echo("Failed\n");
+}
+mysqli_free_result($result);
+
+
+
+
+
+
+
+
+
+
+
+
+$connection->close();
+?>
