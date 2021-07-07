9cb8b06e48d058fe8d76272669f1f5cdfae5f1df
Harsh Rimza <60133190+HarshRimza@users.noreply.github.com>
2021-07-07 18:55:37 +0530

    Update eg19.php

--- eg19.php
+++ eg19.php
@@ -21,7 +21,21 @@ echo("Failed\n");
 }
 mysqli_free_result($result);
 
+// for a particular course
+select 100*(select count(*) from student where course_code=5)/count(*) as course_percent from student;
 
+// for all course percent
+select title,(100*(select count(*) from student where course_code=course.code)/(select count(*) from student)) as course_percents from course;
+
+// for all city percent
+select title,(100*(select count(*) from student where city_code=city.code)/(select count(*) from student)) as city_percents from city;
+
+//for both city and course
+select city.title,course.title,(100*(select count(*) from student where city_code=city.code and course_code=course.code)/(select count(*) from student)) as percents from city,course;
+
+// for both gender=M and city
+
+select city.title,(100*(select count(*) from student where city_code=city.code and gender='M')/(select count(*) from student)) as percents from city;
 
 
 
